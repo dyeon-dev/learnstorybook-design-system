05bfb718384d3b1cfd3013d395d0b814917e5362
kimdayeon37 <kdy37912@naver.com>
2024-11-20 15:26:30 +0900

    globalstyle 추가

--- src/shared/global.js
+++ src/shared/global.js
@@ -1,4 +1,4 @@
-import { css } from '@emotion/react';
+import { css, Global } from '@emotion/react';
 import { color, typography } from './styles';
 
 export const bodyStyles = css`
@@ -9,7 +9,6 @@ export const bodyStyles = css`
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   -webkit-tap-highlight-color: rgba(0, 0, 0, 0);
-  -webkit-tap-highlight-color: transparent;
   -webkit-overflow-scrolling: touch;
 
   * {
@@ -35,83 +34,10 @@ export const bodyStyles = css`
     font-family: ${typography.type.primary};
   }
 
-  sub,
-  sup {
-    font-size: 0.8em;
-  }
-
-  sub {
-    bottom: -0.2em;
-  }
-
-  sup {
-    top: -0.2em;
-  }
-
   b,
   em {
     font-weight: ${typography.weight.bold};
   }
-
-  hr {
-    border: none;
-    border-top: 1px solid ${color.border};
-    clear: both;
-    margin-bottom: 1.25rem;
-  }
-
-  code,
-  pre {
-    font-family: ${typography.type.code};
-    font-size: ${typography.size.s2 - 1}px;
-    -webkit-font-smoothing: antialiased;
-    -moz-osx-font-smoothing: grayscale;
-  }
-
-  code {
-    display: inline-block;
-    padding-left: 2px;
-    padding-right: 2px;
-    vertical-align: baseline;
-
-    color: ${color.secondary};
-  }
-
-  pre {
-    line-height: 18px;
-    padding: 11px 1rem;
-    white-space: pre-wrap;
-
-    background: rgba(0, 0, 0, 0.05);
-    color: ${color.darkest};
-    border-radius: 3px;
-    margin: 1rem 0;
-  }
-
-  &.ReactModal__Body--open {
-    overflow: hidden;
-    &.hide-intercom #intercom-container {
-      display: none;
-    }
-  }
-
-  .ReactModalPortal > div {
-    opacity: 0;
-  }
-
-  .ReactModalPortal .ReactModal__Overlay {
-    transition: all 200ms ease-in;
-
-    &--after-open {
-      opacity: 1;
-    }
-    &--before-close {
-      opacity: 0;
-    }
-  }
 `;
-export const GlobalStyle = css`
-  body {
-    ${bodyStyles}
-  }
-`;
+
+export const GlobalStyle = () => <Global styles={bodyStyles} />;
